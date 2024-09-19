c27e973c0d8c18738e8baee386b0bbdb74480910
Mindaugas <mcesnauskas@gmail.com>
2024-09-19 22:27:48 +0300

    Clean up Flight class

--- src/main/java/lt/mindaugas/scraper/testing/Flight.java
+++ src/main/java/lt/mindaugas/scraper/testing/Flight.java
@@ -1,5 +1,10 @@
 package lt.mindaugas.scraper.testing;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Flight {
     private String airportDeparture;
     private String airportArrival;
@@ -7,42 +12,15 @@ public class Flight {
     private String timeArrival;
     private String flightNumber;
 
-    public Flight(String airportDeparture, String airportArrival, String timeDeparture, String timeArrival, String flightNumber) {
+    public Flight(String airportDeparture,
+                  String airportArrival,
+                  String timeDeparture,
+                  String timeArrival,
+                  String flightNumber) {
         this.airportDeparture = airportDeparture;
         this.airportArrival = airportArrival;
         this.timeDeparture = timeDeparture;
         this.timeArrival = timeArrival;
         this.flightNumber = flightNumber;
-    }
-
-    public String getAirportDeparture() {
-        return airportDeparture;
-    }
-
-    public String getAirportArrival() {
-        return airportArrival;
-    }
-
-    public String getTimeDeparture() {
-        return timeDeparture;
-    }
-
-    public String getTimeArrival() {
-        return timeArrival;
-    }
-
-    public String getFlightNumber() {
-        return flightNumber;
-    }
-
-    @Override
-    public String toString() {
-        return "Flight{" +
-                "airportDeparture='" + airportDeparture + '\'' +
-                ", airportArrival='" + airportArrival + '\'' +
-                ", timeDeparture='" + timeDeparture + '\'' +
-                ", timeArrival='" + timeArrival + '\'' +
-                ", flightNumber='" + flightNumber + '\'' +
-                '}';
     }
 }
